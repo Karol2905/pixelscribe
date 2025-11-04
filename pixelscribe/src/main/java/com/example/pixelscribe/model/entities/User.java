188c1a888cd350ffc1996abb0f250763b1b07ab2
unknown <karol.estupinan-v@labinfo.is.escuelaing.edu.co>
2025-11-04 15:32:53 -0500

    jwt autenticacion

--- pixelscribe/src/main/java/com/example/pixelscribe/model/entities/User.java
+++ pixelscribe/src/main/java/com/example/pixelscribe/model/entities/User.java
@@ -1,26 +1,48 @@
 package com.example.pixelscribe.model.entities;
 
 
-import lombok.Getter;
-import lombok.NoArgsConstructor;
-import lombok.Setter;
+
 import org.springframework.data.annotation.Id;
 import org.springframework.data.mongodb.core.mapping.Document;
 
-import java.time.LocalDateTime;
-
-@Getter
-@Setter
-@NoArgsConstructor
-@Document(collection = "users")
+@Document(collection = "Users")
 public class User {
+
     @Id
-    private String id;
+    private String userId;
 
-    private String name;
     private String email;
-    private LocalDateTime createdAt;
-    private LocalDateTime updatedAt;
+    private String password;
 
+    public User() {}
 
+    public User(String email, String password) {
+        this.email = email;
+        this.password = password;
+    }
+
+    // Getters y Setters
+    public String getUserId() {
+        return userId;
+    }
+
+    public void setUserId(String userId) {
+        this.userId = userId;
+    }
+
+    public String getEmail() {
+        return email;
+    }
+
+    public void setEmail(String email) {
+        this.email = email;
+    }
+
+    public String getPassword() {
+        return password;
+    }
+
+    public void setPassword(String password) {
+        this.password = password;
+    }
 }
