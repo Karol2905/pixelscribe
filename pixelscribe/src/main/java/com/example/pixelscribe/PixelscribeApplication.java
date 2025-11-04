df97f5502df9b14fde473c1d0a44621584d08a27
unknown <karol.estupinan-v@labinfo.is.escuelaing.edu.co>
2025-11-04 14:28:53 -0500

    spring inicio

---
+++ pixelscribe/src/main/java/com/example/pixelscribe/PixelscribeApplication.java
@@ -0,0 +1,13 @@
+package com.example.pixelscribe;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PixelscribeApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(PixelscribeApplication.class, args);
+	}
+
+}
---
+++ pixelscribe/src/test/java/com/example/pixelscribe/PixelscribeApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.pixelscribe;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PixelscribeApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
